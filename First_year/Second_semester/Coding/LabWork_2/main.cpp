731319e7d1ee95a1f956e439b7c2075ece9660d2
Adam Heine <alekskosarev1@gmail.com>
2022-03-10 00:09:51 +0300

    Work updated

--- First_year/Second_semester/Coding/LabWork_2/main.cpp
+++ First_year/Second_semester/Coding/LabWork_2/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class SuperClass {
+public:
+    int a;
+    int b;
+};
+
 int main() {
-    cout << "Hello, World!" << endl;
+    SuperClass test {};
+    test.a = 1;
+    test.b = 2;
+
+    cout << "A = " << test.a << ", B = " << test.b;
+
     return 0;
 }

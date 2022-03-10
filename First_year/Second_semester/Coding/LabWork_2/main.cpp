cea5b625a2ff6e95c5472b5ecb614ffbb94b4036
Adam Heine <alekskosarev1@gmail.com>
2022-03-10 22:30:22 +0300

    Work updated.

--- First_year/Second_semester/Coding/LabWork_2/main.cpp
+++ First_year/Second_semester/Coding/LabWork_2/main.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
 using namespace std;
 
-class SuperClass {
-public:
-    SuperClass() = default;
-
+class TimeDate {
+private:
     int a;
     int b;
 
-    void print() const {
-        cout << "A = " << a << ", " << "B = " << b << endl;
-    }
+public:
+    TimeDate() = default;
 };
 
 int main() {
-    SuperClass test {};
-    test.a = 2;
-    test.b = 3;
-
-    test.print();
 
     return 0;
 }

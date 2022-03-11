4fea7150fba2c07efa089975ade794935fe9afa0
Adam Heine <alekskosarev1@gmail.com>
2022-03-11 15:43:27 +0300

    Work updated.

--- First_year/Second_semester/Coding/LabWork_2/main.cpp
+++ First_year/Second_semester/Coding/LabWork_2/main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 class TimeDate {
+
 private:
-    int a;
-    int b;
+    struct time {
+        short int seconds;
+        short int minutes;
+        short int hours;
+    };
+
+    struct date {
+        short int days;
+        short int months;
+        short int years;
+    };
 
 public:
     TimeDate() = default;
+    TimeDate(string value) {
+
+    }
+
 };
 
 int main() {

d6d97e1f93347b90d79dfe88c06069ffd5927c03
Adam Heine <alekskosarev1@gmail.com>
2022-03-11 19:30:16 +0300

    Work updated.

--- First_year/Second_semester/Coding/LabWork_2/main.cpp
+++ First_year/Second_semester/Coding/LabWork_2/main.cpp
@@ -8,15 +8,15 @@ class TimeDate {
 
 private:
     struct time {
-        short seconds;
-        short minutes;
-        short hours;
+        int seconds;
+        int minutes;
+        int hours;
     };
 
     struct date {
-        short days;
-        short months;
-        short years;
+        int days;
+        int months;
+        int years;
     };
 
     string value;
@@ -45,37 +45,66 @@ public:
     };
 
 
-    size_t str_len() {
+    [[maybe_unused]] size_t str_len() {
         const char *c_value = value.c_str();
         return strlen(c_value);
     }
 
-    void dismemberment() {
-        size_t dots = 0;
-        string tmp;
+    [[maybe_unused]] void dismemberment() {
+        short dots = 0;
+        short parameter = 0; // 0 - sec/day, 1 - min/month, 2 - hour/year
 
+        string tmp;
         tmp.clear();
 
         for (size_t i = 0; i <= str_len(); i++) {
-            if (dots <= 3) {
-                if (value[i] == '.') {
-                    dots++;
+            if (dots < 3) {
+                if (value[i] != '.') {
+                    tmp += value[i];
                 } else {
+                    if (!parameter) {
+                        duration.hours = stoi(tmp);
+                        tmp.clear();
 
+                        parameter++;
+                    } else if (parameter == 1) {
+                        duration.minutes = stoi(tmp);
+                        tmp.clear();
+
+                        parameter++;
+                    } else if (parameter == 2) {
+                        duration.seconds = stoi(tmp);
+                        tmp.clear();
+                    }
                 }
             } else {
                 cout << "Error: invalid time or date format.";
+                tmp.clear();
+
                 return;
             }
+        }
+    }
+
+    [[maybe_unused]] void print(bool is_time) const {
+        if (is_time) {
+            cout << duration.seconds << '.' << duration.minutes << '.' << duration.hours << endl;
+        } else {
+            cout << period.days << '.' << period.months << '.' << period.years << endl;
         }
     }
 
 };
 
 int main() {
-    string str;
-    str.clear();
-    cout << str.empty();
+    string rmp;
+    rmp.clear();
+
+    rmp += '1';
+    rmp += '2';
+
+    int val = stoi(rmp);
+    cout << val;
 
     return 0;
 }

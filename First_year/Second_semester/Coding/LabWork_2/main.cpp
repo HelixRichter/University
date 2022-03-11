9898ab3666f57f6b62684a0d502c86f74681bb1e
Adam Heine <alekskosarev1@gmail.com>
2022-03-11 17:53:40 +0300

    Work updated.

--- First_year/Second_semester/Coding/LabWork_2/main.cpp
+++ First_year/Second_semester/Coding/LabWork_2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <string>
 
 using namespace std;
@@ -7,26 +8,74 @@ class TimeDate {
 
 private:
     struct time {
-        short int seconds;
-        short int minutes;
-        short int hours;
+        short seconds;
+        short minutes;
+        short hours;
     };
 
     struct date {
-        short int days;
-        short int months;
-        short int years;
+        short days;
+        short months;
+        short years;
     };
 
-public:
-    TimeDate() = default;
-    TimeDate(string value) {
+    string value;
+    time duration {};
+    date period {};
 
+public:
+    TimeDate() {
+        duration.seconds = 0;
+        duration.minutes = 0;
+        duration.hours = 0;
+
+        period.days = 0;
+        period.months = 0;
+        period.years = 0;
+
+        value.clear();
+    };
+
+    [[maybe_unused]] explicit TimeDate(const string& str_value) {
+        value.assign(str_value);
+    };
+
+    [[maybe_unused]] TimeDate(const TimeDate & copied_value) {
+
+    };
+
+
+    size_t str_len() {
+        const char *c_value = value.c_str();
+        return strlen(c_value);
+    }
+
+    void dismemberment() {
+        size_t dots = 0;
+        string tmp;
+
+        tmp.clear();
+
+        for (size_t i = 0; i <= str_len(); i++) {
+            if (dots <= 3) {
+                if (value[i] == '.') {
+                    dots++;
+                } else {
+
+                }
+            } else {
+                cout << "Error: invalid time or date format.";
+                return;
+            }
+        }
     }
 
 };
 
 int main() {
+    string str;
+    str.clear();
+    cout << str.empty();
 
     return 0;
 }

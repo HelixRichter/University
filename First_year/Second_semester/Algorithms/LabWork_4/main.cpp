1d7589107c92d5e3938f9bc16aea676214de8109
Adam Heine <alekskosarev1@gmail.com>
2022-03-14 20:17:39 +0300

    Some works updated.

--- First_year/Second_semester/Algorithms/LabWork_4/main.cpp
+++ First_year/Second_semester/Algorithms/LabWork_4/main.cpp
@@ -2,90 +2,90 @@
 using namespace std;
 
 class Elem {
-	public:
-		int x;
-		Elem* next;
+public:
+    int x;
+    Elem* next;
 };
 
 class Stack {
-	public:
-		Elem* head;
+public:
+    Elem* head;
 
-		Stack() {
-			head = NULL;
-		}
+    Stack() {
+        head = nullptr;
+    }
 
-	void print() {
-		Elem* el = head;
-		while(el) {
-			cout << el -> x << " ";
-			el = el -> next;
-		}
-		cout << endl;
-	}
+    void print() const {
+        Elem* el = head;
+        while(el) {
+            cout << el -> x << " ";
+            el = el -> next;
+        }
+        cout << endl;
+    }
 
-	void add(int x) {
-		Elem* el = new(Elem);
-		el -> x = x;
-		el -> next = head;
-		head = el;
-	}
+    void add(int x) {
+        Elem* el = new(Elem);
+        el -> x = x;
+        el -> next = head;
+        head = el;
+    }
 
-	bool isEmpty() {
-		return (!(head == NULL));
-	}
+    [[nodiscard]] bool isEmpty() const {
+        return head != nullptr;
+    }
 
-	Elem* get() {
-		if(isEmpty()) {
-			Elem* el = head;
-			head = el -> next;
-			while(el -> next) {
-				el -> x = el -> next -> x;
-				el -> next = el -> next -> next;
-				el = el -> next;
-			}
+    Elem* get() {
+        if(isEmpty()) {
+            Elem* el = head;
+            head = el -> next;
+            while(el -> next) {
+                el -> x = el -> next -> x;
+                el -> next = el -> next -> next;
+                el = el -> next;
+            }
 
-			return head;
-		} else {
-			cout << "GET() ERR" << endl;
+            return head;
+        } else {
+            cout << "GET() ERR" << endl;
 
-			return head;
-		}
-	}
+            return head;
+        }
+    }
 
-	Elem* readHead() {
-		return head;
-	}
+    [[maybe_unused]] [[nodiscard]] Elem* readHead() const {
+        return head;
+    }
 };
 
 int main() {
-	Stack st;
-	cout << "Is stack empty now? " << st.isEmpty() << endl;
+    Stack st;
+    cout << "Is stack empty now? " << st.isEmpty() << endl;
 
-	cout << "I will add " << 12 << " element." << endl;
-	st.add(12);
-	cout << "Here is your stack now:" << endl;
-	st.print();
+    cout << "I will add " << 12 << " element." << endl;
+    st.add(12);
+    cout << "Here is your stack now:" << endl;
+    st.print();
 
-	cout << "I will add " << 26 << " element." << endl;
-	st.add(26);
-	cout << "Here is your stack now:" << endl;
-	st.print();
+    cout << "I will add " << 26 << " element." << endl;
+    st.add(26);
+    cout << "Here is your stack now:" << endl;
+    st.print();
 
-	cout << "I will add " << 30 << " element." << endl;
-	st.add(30);
-	cout << "Here is your stack now:" << endl;
-	st.print();
+    cout << "I will add " << 30 << " element." << endl;
+    st.add(30);
+    cout << "Here is your stack now:" << endl;
+    st.print();
 
-	cout << "I will use get() now." << endl;
-	st.get();
-	cout << "Here is your stack now:" << endl;
-	st.print();
+    cout << "I will use get() now." << endl;
+    st.get();
+    cout << "Here is your stack now:" << endl;
+    st.print();
 
-	cout << "I will add " << 56 << " element." << endl;
-	st.add(56);
-	cout << "Here is your stack now:" << endl;
-	st.print();
+    cout << "I will add " << 56 << " element." << endl;
+    st.add(56);
+    cout << "Here is your stack now:" << endl;
+    st.print();
 
-	cout << "Is stack empty now? " << st.isEmpty() << endl;
+    cout << "Is stack empty now? " << st.isEmpty() << endl;
 }

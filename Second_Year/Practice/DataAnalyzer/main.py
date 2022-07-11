d1947cf77915c88db08ccb5fa5f830e924fbea2b
AdamHeine <alekskosarev1@gmail.com>
2022-07-11 14:50:20 +0300

    New version [1.3] has been released!

--- Second_Year/Practice/DataAnalyzer/main.py
+++ Second_Year/Practice/DataAnalyzer/main.py
@@ -2,7 +2,7 @@ from PyQt6.QtWidgets import QApplication
 from PyQt6.QtWidgets import QWidget
 
 
-app = QApplication([]) # Передаём [], чтобы не использовать аргументы командной строки для приложения
+app = QApplication([])  # Передаём [], чтобы не использовать аргументы командной строки для приложения
 
 window = QWidget()  # Создаём виджет Qt — окно.
 window.show()  # Окно по умолчанию скрыто.

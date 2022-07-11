c4d25f9b76bc4dd90979b895b12419e0de126c65
AdamHeine <alekskosarev1@gmail.com>
2022-07-11 14:46:24 +0300

    New version [1.2] has been released!

--- Second_Year/Practice/DataAnalyzer/main.py
+++ Second_Year/Practice/DataAnalyzer/main.py
@@ -1,18 +1,10 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QWidget
 
-import sys  # Только для доступа к аргументам командной строки
 
-# Приложению нужен один (и только один) экземпляр QApplication.
-# Передаём sys.argv, чтобы разрешить аргументы командной строки для приложения.
-# Если не будете использовать аргументы командной строки, QApplication([]) тоже работает
-app = QApplication(sys.argv)
+app = QApplication([]) # Передаём [], чтобы не использовать аргументы командной строки для приложения
 
-# Создаём виджет Qt — окно.
-window = QWidget()
-window.show()  # Важно: окно по умолчанию скрыто.
+window = QWidget()  # Создаём виджет Qt — окно.
+window.show()  # Окно по умолчанию скрыто.
 
-# Запускаем цикл событий.
-app.exec()
-
-# Приложение не доберётся сюда, пока вы не выйдете и цикл
-# событий не остановится.
+app.exec()  # Запускаем цикл событий.
